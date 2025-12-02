5843b9de133bc286ce584ab470d55e31abfde8d2
YahyaMeddeb <yahyameddeb12@gmail.com>
2025-12-02 20:54:28 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class DevMobMvcApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'DEVMOB MVC',
         theme: ThemeData(
-          
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal), useMaterial3: true,
         ),
         
       ),
