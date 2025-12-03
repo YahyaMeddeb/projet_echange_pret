3282dbe6147b6b806ea079376329a024906f8fd1
YahyaMeddeb <yahyameddeb12@gmail.com>
2025-12-03 21:39:05 +0100

    update

--- lib/models/item.dart
+++ lib/models/item.dart
@@ -6,12 +6,4 @@ class Item {
   final double pricePerDay;
   final String status;
 
-  Item({
-    required this.id,
-    required this.ownerId,
-    required this.title,
-    required this.description,
-    required this.pricePerDay,
-    this.status = 'available',
-  });
-}
+  
