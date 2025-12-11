57799c8da3bf098be3d4cdc07ee70f512ab4d465
YahyaMeddeb <yahyameddeb12@gmail.com>
2025-12-11 19:45:08 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -11,17 +11,7 @@ void main() {
 class DevMobMvcApp extends StatelessWidget {
   const DevMobMvcApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => ItemController()..loadItems(),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'DEVMOB MVC',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal), useMaterial3: true,
-        ),
-        
+
       ),
     );
   }
